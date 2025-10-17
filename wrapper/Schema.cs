32c326ad4dccfaf10b0fdf53c72c9ac80b3815d0
Fallenwood <fallenwood.y@outlook.com>
2025-10-17 20:31:29 +0800

    Add wrapper project

---
+++ wrapper/Schema.cs
@@ -0,0 +1,43 @@
+namespace Schema;
+
+using System.Text.Json.Serialization;
+
+public sealed class OpenSolutionNotifiation(string solution)
+{
+    [JsonPropertyName("jsonrpc")]
+    public string JsonRpc { get; } = "2.0";
+    [JsonPropertyName("method")]
+    public string Method { get; } = "solution/open";
+    [JsonPropertyName("params")]
+    public SolutionParams Params { get; } = new(solution);
+}
+
+public sealed class OpenProjectNotification(string project)
+{
+    [JsonPropertyName("jsonrpc")]
+    public string JsonRpc { get; } = "2.0";
+
+    [JsonPropertyName("method")]
+    public string Method { get; } = "project/open";
+
+    [JsonPropertyName("params")]
+    public ProjectParams Params { get; } = new(project);
+}
+
+public sealed class SolutionParams(string solution)
+{
+    [JsonPropertyName("solution")]
+    public string Solution { get; } = solution;
+}
+
+public sealed class ProjectParams(string project)
+{
+    [JsonPropertyName("project")]
+    public string Project { get; } = project;
+}
+
+[JsonSerializable(typeof(OpenSolutionNotifiation))]
+[JsonSerializable(typeof(OpenProjectNotification))]
+public partial class LspJsonSerializerContext : JsonSerializerContext
+{
+}
