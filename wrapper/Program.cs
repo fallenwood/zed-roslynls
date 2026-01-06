08db6ae8a485ca0014f9af79b4d6d72c5b4dfa7d
Fallenwood <fallenwood.y@outlook.com>
2026-01-06 11:11:58 +0800

    Fix debugger

--- wrapper/Program.cs
+++ wrapper/Program.cs
@@ -6,9 +6,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using ZedRoslynLS;
 
-// #if false
+#if DEBUG
 Debugger.Launch();
-// #endif
+#endif
 
 await ConsoleApp.RunAsync(args,
     static async (string lsp, string projectRoot, string? logFilePath = null, RpcType wrapperRpcType = RpcType.Stdio, RpcType lspRpcType = RpcType.NamedPipe, CancellationToken cancellationToken = default) =>
